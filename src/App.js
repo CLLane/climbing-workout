6e2f6fec61d375d7fd158dd63943ad719f0bb1f0
Chris Lane <pankokrumbs@gmail.com>
2020-06-24 18:40:21 -0600

    Refactor App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
