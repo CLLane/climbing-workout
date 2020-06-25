bd91867689aee2d00ebea650187c703b94039f53
Chris Lane <pankokrumbs@gmail.com>
2020-06-25 13:01:44 -0600

    Create props

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,20 +1,24 @@
 import React, { Component } from "react";
 import "./App.css";
+import {Nav} from "../Nav/Nav";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-
+      navState: null,
     };
   }
 
-  
+  updateNavState = (navSelection) => {
+    this.setState({...state, navState: navSelection})
+  }
 
   render() {
     return (
     <div>
       <h1>Bouldering Training</h1>
+      <Nav updateNavState={this.updateNavState}></Nav>
     </div>
     );
   }
--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -1,2 +1,10 @@
-import React, {Component} from 'react';
+import React from 'react';
 import "./Nav.css";
+const {updateNavState} = this.props
+export const Nav = () => {
+  return (
+    <nav>
+      <p onClick={() => updateNavState("4x4")}>Four by Fours</p>
+    </nav>
+  )
+}
